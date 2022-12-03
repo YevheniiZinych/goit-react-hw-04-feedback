0ce195524ff7d69d92cc6ae05b77070990240de5
ZEugenii <zinich911@gmail.com>
2022-12-03 10:43:05 +0200

    Update FeedbackOptions.js

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { FeedbackBtn, Wrapper } from './FeedbackOptions.style';
+import { FeedbackBtn } from './FeedbackOptions.style';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return options.map(option => {
